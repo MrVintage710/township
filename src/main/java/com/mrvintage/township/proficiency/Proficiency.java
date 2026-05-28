a0ec4ded9dbee7e3db4c5e55032e04290d75feec
Brooks Palin <brookspalin710@gmail.com>
2026-05-28 09:44:30 -0600

    initial commit

---
+++ src/main/java/com/mrvintage/township/proficiency/Proficiency.java
@@ -0,0 +1,24 @@
+﻿package com.mrvintage.township.proficiency;
+
+import net.minecraft.core.HolderLookup;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.Tag;
+import net.neoforged.neoforge.attachment.IAttachmentSerializer;
+import net.neoforged.neoforge.common.util.INBTSerializable;
+
+public class Proficiency implements INBTSerializable {
+
+
+
+    @Override
+    public Tag serializeNBT(HolderLookup.Provider provider) {
+        var tag = new CompoundTag();
+
+        return tag;
+    }
+
+    @Override
+    public void deserializeNBT(HolderLookup.Provider provider, Tag tag) {
+
+    }
+}
